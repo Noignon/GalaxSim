bff8ed0dc2e0835d46a5a7055a005007d3fa7682
anaofind <leo.rauzier8@etu.univ-lorraine.fr>
2019-06-02 19:12:58 +0200

    correction filtre de coordonnées

--- src/fr/istic/galaxsim/data/Filter.java
+++ src/fr/istic/galaxsim/data/Filter.java
@@ -18,14 +18,34 @@ public class Filter {
 	private static boolean filterMassActived = false;
 	
 	/**
-	 * boolean indiquant si le filtre de longitude est activé ou pas
-	 */
-	private static boolean filterLonActived = false;
+	 * boolean indiquant si le filtre de coordonnée x min est activé
+	 */ 
+	private static boolean filterCoordinateXMinActived = false;
 	
 	/**
-	 * boolean indiquant si le filtre de latitude est activé ou pas
-	 */
-	private static boolean filterLatActived = false;
+	 * boolean indiquant si le filtre de coordonnée x max est activé
+	 */ 
+	private static boolean filterCoordinateXMaxActived = false;
+	
+	/**
+	 * boolean indiquant si le filtre de coordonnée y min est activé
+	 */ 
+	private static boolean filterCoordinateYMinActived = false;
+	
+	/**
+	 * boolean indiquant si le filtre de coordonnée y max est activé
+	 */ 
+	private static boolean filterCoordinateYMaxActived = false;
+	
+	/**
+	 * boolean indiquant si le filtre de coordonnée z min est activé
+	 */ 
+	private static boolean filterCoordinateZMinActived = false;
+	
+	/**
+	 * boolean indiquant si le filtre de coordonnée z max est activé
+	 */ 
+	private static boolean filterCoordinateZMaxActived = false;
 	
 	/**
 	 * boolean indiquant si le filtre de marge d'erreur est activé ou pas
@@ -48,14 +68,19 @@ public class Filter {
 	private static double deviationUncertainty = -1;
 	
 	/**
-	 * filtre de longitude super galactique
+	 * filtre de la coordonnée x
 	 */
-	private static double superGalacticLonMin = -1, superGalacticLonMax = -1;
+	private static double xMin = -1, xMax = -1;
 	
 	/**
-	 * filtre de latitude super galactique
+	 * filtre de la coordonnée y
 	 */
-	private static double superGalacticLatMin = -1, superGalacticLatMax = -1;
+	private static double yMin = -1, yMax = -1;
+	
+	/**
+	 * filtre de la coordonnée z
+	 */
+	private static double zMin = -1, zMax = -1;
 	
 	/**
 	 * méthode permettant de savoir si un amas correspond bien au filtres
@@ -63,7 +88,7 @@ public class Filter {
 	 * @return boolean
 	 */
 	public static boolean goodAmas(Amas a){
-		return (goodDistance(a) && goodMassAmas(a) && goodDeviationUncertainty(a) && goodLon(a) && goodLat(a));
+		return (goodDistance(a) && goodMassAmas(a) && goodDeviationUncertainty(a));
 	}
 	
 	/**
@@ -72,7 +97,16 @@ public class Filter {
 	 * @return boolean
 	 */
 	public static boolean goodGalaxies(Galaxy g){
-		return (goodDistance(g) && goodDeviationUncertainty(g) && goodLon(g) && goodLat(g));
+		return (goodDistance(g) && goodDeviationUncertainty(g));
+	}
+	
+	/**
+	 * méthode permettant de savoir si un objet à une coordonnée correcte
+	 * @param cosmosElement l'objet
+	 * @return boolean
+	 */
+	public static boolean goodCoordinate(CosmosElement cosmosElement) {
+		return (goodCoordinateX(cosmosElement) && goodCoordinateY(cosmosElement) && goodCoordinateZ(cosmosElement));
 	}
 	
 	/**
@@ -103,37 +137,57 @@ public class Filter {
 	}
 	
 	/**
-	 * méthode permettant de changer le fltre de longitude min et max
-	 * @param sgLon1 la longitude super galactique 1
-	 * @param sgLon2 la longitude super galactique 2
+	 * méthode permettant de changer le filtre de coordonnée x min
+	 * @param x le x min
 	 */
-	public static void setLonFilter(double sgLon1, double sgLon2){
-		superGalacticLonMin = sgLon1;
-		superGalacticLonMax = sgLon2;	
-		
-		if (sgLon2 < sgLon1) {
-			superGalacticLonMin = sgLon2;
-			superGalacticLonMax = sgLon1;
-		}
-		
-		filterLonActived = true;
+	public static void setCoordinateXMinFilter(double x){
+		xMin = x;
+		filterCoordinateXMinActived = true;
 	}
 	
 	/**
-	 * méthode permettant de changer le fltre de latitude min et max
-	 * @param sgLat1 la latitude super galactique 1
-	 * @param sgLat2 la latitude super galactique 2
+	 * méthode permettant de changer le filtre de coordonnée x max
+	 * @param x le x max
 	 */
-	public static void setLatFilter(double sgLat1, double sgLat2){
-		superGalacticLatMin = sgLat1;
-		superGalacticLatMax = sgLat2;
-		
-		if (sgLat2 < sgLat1) {
-			superGalacticLatMin = sgLat2;
-			superGalacticLatMax = sgLat1;
-		}
-		
-		filterLatActived = true;
+	public static void setCoordinateXMaxFilter(double x){
+		xMax = x;
+		filterCoordinateXMaxActived = true;
+	}
+	
+	/**
+	 * méthode permettant de changer le filtre de coordonnée y min
+	 * @param y le y min
+	 */
+	public static void setCoordinateYMinFilter(double y){
+		yMin = y;
+		filterCoordinateYMinActived = true;
+	}
+	
+	/**
+	 * méthode permettant de changer le filtre de coordonnée y max
+	 * @param y le y max
+	 */
+	public static void setCoordinateYMaxFilter(double y){
+		yMax = y;
+		filterCoordinateYMaxActived = true;
+	}
+	
+	/**
+	 * méthode permettant de changer le filtre de coordonnée z min
+	 * @param z le z min
+	 */
+	public static void setCoordinateZMinFilter(double z){
+		zMin = z;
+		filterCoordinateZMinActived = true;
+	}
+	
+	/**
+	 * méthode permettant de changer le filtre de coordonnée z max
+	 * @param z le z max
+	 */
+	public static void setCoordinateZMaxFilter(double z){
+		zMax = z;
+		filterCoordinateZMaxActived = true;
 	}
 	
 	/**
@@ -142,17 +196,17 @@ public class Filter {
 	public static void removeAllFilter(){
 		distance = -1;
 		mass = -1;
-		superGalacticLonMin = -1;
-		superGalacticLonMax = -1;
-		superGalacticLatMin = -1;
-		superGalacticLatMax = -1;
 		deviationUncertainty = -1;
 		
 		filterDistanceActived = false;
 		filterMassActived = false;
 		filterDeviationUncertaintyActived = false;
-		filterLonActived = false;
-		filterLatActived = false;
+		filterCoordinateXMinActived = false;
+		filterCoordinateXMaxActived = false;
+		filterCoordinateYMinActived = false;
+		filterCoordinateYMaxActived = false;
+		filterCoordinateZMinActived = false;
+		filterCoordinateZMaxActived = false;
 	}
 	
 	/**
@@ -192,26 +246,77 @@ public class Filter {
 	}
 	
 	/**
-	 * methode permettant de savoir si un objet a la bonne longitude super galatique
+	 * méthode permettant de savoir si un objet à une coordonnée x correct
 	 * @param cosmosElement l'objet
 	 * @return boolean
 	 */
-	private static boolean goodLon(CosmosElement cosmosElement) {
-		if (cosmosElement.getSuperGalacticLon() >= superGalacticLonMin && cosmosElement.getSuperGalacticLon() <= superGalacticLonMax) {
-			return true;
+	private static boolean goodCoordinateX(CosmosElement cosmosElement){
+		boolean min = !filterCoordinateXMinActived;
+		boolean max = !filterCoordinateXMaxActived;
+		
+		Coordinate c = cosmosElement.getCoordinate(0);
+		
+		if (c != null) {
+			if (filterCoordinateXMinActived) {
+				min = (c.getX() >= xMin);
+			}
+			if (filterCoordinateXMaxActived) {
+				max = (c.getX() <= xMax);
+			}
+			
+			return (min && max);	
 		}
-		return !filterLonActived;
+		
+		return false;
 	}
 	
 	/**
-	 * methode permettant de savoir si un objet a la bonne latitude super galatique
+	 * méthode permettant de savoir si un objet à une coordonnée y correct
 	 * @param cosmosElement l'objet
 	 * @return boolean
 	 */
-	private static boolean goodLat(CosmosElement cosmosElement) {
-		if (cosmosElement.getSuperGalacticLat() >= superGalacticLatMin && cosmosElement.getSuperGalacticLat() <= superGalacticLatMax) {
-			return true;
+	private static boolean goodCoordinateY(CosmosElement cosmosElement){
+		boolean min = !filterCoordinateYMinActived;
+		boolean max = !filterCoordinateYMaxActived;
+		
+		Coordinate c = cosmosElement.getCoordinate(0);
+		
+		if (c != null) {
+			if (filterCoordinateYMinActived) {
+				min = (c.getY() >= yMin);
+			}
+			if (filterCoordinateYMaxActived) {
+				max = (c.getY() <= yMax);
+			}
+			
+			return (min && max);	
 		}
-		return !filterLatActived;
-	}	
+		
+		return false;
+	}
+	
+	/**
+	 * méthode permettant de savoir si un objet à une coordonnée z correct
+	 * @param cosmosElement l'objet
+	 * @return boolean
+	 */
+	private static boolean goodCoordinateZ(CosmosElement cosmosElement){
+		boolean min = !filterCoordinateZMinActived;
+		boolean max = !filterCoordinateZMaxActived;
+		
+		Coordinate c = cosmosElement.getCoordinate(0);
+		
+		if (c != null) {
+			if (filterCoordinateZMinActived) {
+				min = (c.getZ() >= zMin);
+			}
+			if (filterCoordinateZMaxActived) {
+				max = (c.getZ() <= zMax);
+			}
+			
+			return (min && max);
+		}
+		
+		return false;
+	}
 }
