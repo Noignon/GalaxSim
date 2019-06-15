06fa19171d152d85b35f6efd7351428018ee37be
Aku64 <45423313+Aku64@users.noreply.github.com>
2019-06-15 13:33:15 +0200

    ajout attraction amas proches

--- src/fr/istic/galaxsim/calcul/CalcsProcessing.java
+++ src/fr/istic/galaxsim/calcul/CalcsProcessing.java
@@ -1,16 +1,55 @@
 package fr.istic.galaxsim.calcul;
 
 import fr.istic.galaxsim.data.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.concurrent.Task;
 
-public class CalcsProcessing {
+public class CalcsProcessing extends Task {
+	
+	public static final double Time = 2 * Math.pow(10, 16);
+	public static final double G = 6.67408 * Math.pow(10, -11);
+	public static final double MsolaireEnKilo = 1.9891 * Math.pow(10, 42);
+	public static final double MparsecEnMetre = 3.086 * Math.pow(10, 22);
 
 	private static final int T = 10;
 
-	public static void process() {
-		// Cette ligne ne sert theoriquement pas
-		// DataBase.sortAmas(DataBase.SORTING_MASS, true);
+	/**
+	 * Propriete d'avancement mise a jour apres chaque element traite
+	 */
+	public final DoubleProperty progressProperty = new SimpleDoubleProperty();
 
+	/**
+	 * Nombre d'elements a traiter
+	 */
+	private int elementsNumber;
+
+	/**
+	 * Nombre d'elements traites
+	 */
+	private int processedElements = 0;
+
+	@Override
+	protected Object call() throws Exception {
+		progressProperty.set(0.0);
+
+		//tri des amas par ordre decroissant de masse
+		DataBase.sortAmas(DataBase.SORTING_MASS, true);
+		
+		//tableau des amas trié dans l'ordre decroissant de masse
 		Amas[] amas = DataBase.getAllAmas();
+		
+		//tri des amas par ordre croissant de distance
+		DataBase.sortAmas(DataBase.SORTING_DISTANCE, false);	
+		
+		Amas[] amasProche = DataBase.getAllAmas();
+
+		// Calcul du nombre d'elements a traiter :
+		// chaque amas / galaxie possede T coordonnees
+		elementsNumber = T * (DataBase.getAllAmas().length + DataBase.getAllGalaxies().length);
+
+		// Calcul des coordonnees initiales
+		initialCoordsCalculation();
 
 		// boucle pour le nombre d'intervalle de coordonnees
 		for (int t = 0; t < T; t++) {
@@ -20,8 +59,8 @@ public class CalcsProcessing {
 				double sumForceX = 0;
 				double sumForceY = 0;
 				double sumForceZ = 0;
-
-				// boucle pour calculer les forces entre l'amas actuel et tous les autres
+				
+				// boucle pour calculer les forces entre l'amas actuel et les 100 amas les plus massifs
 				for (int i = 0; i < Math.min(amas.length, 100); i++) {
 					Amas a2 = amas[i];
 					if (a1 == a2) {
@@ -35,39 +74,82 @@ public class CalcsProcessing {
 						sumForceZ += CalculAmas.forceZ(a1, a2, t, force);
 					}
 				}
+				
+				// boucle pour calculer les forces entre l'amas actuel et les 100 amas les plus proches
+				for(int i = 0; i < Math.min(amasProche.length, 100); i++) {
+					Amas a2 = amasProche[i];
+					if (a1 == a2) {
+						continue;
+					}
+
+					double force = CalculAmas.forceAttraction(a1, a2, t);
+					if (force != 0) {
+						sumForceX += CalculAmas.forceX(a1, a2, t, force);
+						sumForceY += CalculAmas.forceY(a1, a2, t, force);
+						sumForceZ += CalculAmas.forceZ(a1, a2, t, force);
+					}
+				}
 				CalculAmas.coordByTime(a1, sumForceX, sumForceY, sumForceZ, t);
+				increaseProgress();
 			}
 
+			Galaxy[] g = DataBase.tableGalaxies.toArray(new Galaxy[DataBase.tableGalaxies.size()]);
+			int nbGalaxies = DataBase.getNumberGalaxies();
+			
 			// boucle pour traiter chaque galaxies
-			for (Galaxy g : DataBase.getAllGalaxies()) {
+			for ( int j = 0; j < nbGalaxies; j++) {
 				double sumForceX = 0;
 				double sumForceY = 0;
 				double sumForceZ = 0;
 
-				// boucle pour calculer les forces entre la galaxie actuelle et tous les autres
-				// amas
+				// boucle pour calculer les forces entre la galaxie actuelle et les 100 amas les plus massifs
 				for (int i = 0; i < Math.min(amas.length, 100); i++) {
 					Amas a = amas[i];
-					double force = CalculGalaxies.forceAttraction(g, a, t);
+					double force = CalculGalaxies.forceAttraction(g[j], a, t);
 					if (force != 0) {
-						sumForceX += CalculGalaxies.forceX(g, a, t, force);
-						sumForceY += CalculGalaxies.forceY(g, a, t, force);
-						sumForceZ += CalculGalaxies.forceZ(g, a, t, force);
+						sumForceX += CalculGalaxies.forceX(g[j], a, t, force);
+						sumForceY += CalculGalaxies.forceY(g[j], a, t, force);
+						sumForceZ += CalculGalaxies.forceZ(g[j], a, t, force);
 					}
 				}
-				CalculGalaxies.coordByTime(g, sumForceX, sumForceY, sumForceZ, t);
+				
+				// boucle pour calculer les forces entre la galaxie actuelle et les 100 amas les plus proches
+				for (int i = 0; i < Math.min(amasProche.length, 100); i++) {
+					Amas a = amasProche[i];
+					double force = CalculGalaxies.forceAttraction(g[j], a, t);
+					if (force != 0) {
+						sumForceX += CalculGalaxies.forceX(g[j], a, t, force);
+						sumForceY += CalculGalaxies.forceY(g[j], a, t, force);
+						sumForceZ += CalculGalaxies.forceZ(g[j], a, t, force);
+					}
+				}
+				CalculGalaxies.coordByTime(g[j], sumForceX, sumForceY, sumForceZ, t);
+				increaseProgress();
 			}
 		}
+
+		return null;
 	}
 
-	public static void coordsCalculation() {
-		DataBase.sortAmas(DataBase.SORTING_MASS, true);
-		for (Galaxy g : DataBase.getAllGalaxies()) {
-			double Vx = CalculGalaxies.velocityX(g);
-			double Vy = CalculGalaxies.velocityY(g);
-			double Vz = CalculGalaxies.velocityZ(g);
-			g.addVelocity(new Vector(Vx, Vy, Vz));
-			CalculGalaxies.calculCoordInit(g);
+	/**
+	 * Met a jour la progression de la tache
+	 * La valeur de processedElements est incrementee.
+	 */
+	private void increaseProgress() {
+		progressProperty.setValue(++processedElements / (double) elementsNumber);
+	}
+
+	private void initialCoordsCalculation() {
+		
+		Galaxy[] g = DataBase.tableGalaxies.toArray(new Galaxy[DataBase.tableGalaxies.size()]);
+		int nbGalaxies = DataBase.getNumberGalaxies();
+		
+		for (int i = 0; i< nbGalaxies; i++) {
+			double Vx = CalculGalaxies.velocityX(g[i]);
+			double Vy = CalculGalaxies.velocityY(g[i]);
+			double Vz = CalculGalaxies.velocityZ(g[i]);
+			g[i].addVelocity(new Vector(Vx, Vy, Vz));
+			CalculGalaxies.calculCoordInit(g[i]);
 		}
 
 		for (Amas a : DataBase.getAllAmas()) {
